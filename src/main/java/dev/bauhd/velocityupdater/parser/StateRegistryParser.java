31ed1395c227a0cf1db30bd89301d508bbf50e16
Aaron <bauhd@gmx.net>
2026-09-14 19:23:08 +0200

    fix: correctly negate

--- src/main/java/dev/bauhd/velocityupdater/parser/StateRegistryParser.java
+++ src/main/java/dev/bauhd/velocityupdater/parser/StateRegistryParser.java
@@ -19,7 +19,7 @@ public final class StateRegistryParser extends Parser {
   @Override
   public boolean parse(final CompilationUnit compilationUnit, final MinecraftVersion version) {
     final var stateRegistryEnum = compilationUnit.getEnumByName("StateRegistry");
-    if (stateRegistryEnum.isPresent() && !PacketIdChecker.PACKET_IDS.isEmpty()) {
+    if (stateRegistryEnum.isEmpty() || PacketIdChecker.PACKET_IDS.isEmpty()) {
       return false;
     }
     final var versionConstant = version.toConstant();
