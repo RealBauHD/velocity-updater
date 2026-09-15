e6ab76da85abdefbe841e883ad598c4cf337203e
Aaron <bauhd@gmx.net>
2026-09-15 09:08:04 +0200

    fix: replace packet id if version didn't change

--- src/main/java/dev/bauhd/velocityupdater/parser/StateRegistryParser.java
+++ src/main/java/dev/bauhd/velocityupdater/parser/StateRegistryParser.java
@@ -26,39 +26,45 @@ public final class StateRegistryParser extends Parser {
     compilationUnit.addImport(new ImportDeclaration(
         "com.velocitypowered.api.network.ProtocolVersion." + versionConstant, true, false));
 
-    for (final var enumEntry : stateRegistryEnum.get().getEntries()) {
-      final var name = normalizeStateName(enumEntry.getNameAsString());
+    for (final var stateEntry : stateRegistryEnum.get().getEntries()) {
+      final var name = normalizeStateName(stateEntry.getNameAsString());
       final var ids = PacketIdChecker.PACKET_IDS.get(name);
-      if (ids != null) {
-        final var classBody = enumEntry.getClassBody().getFirst();
-        classBody.ifPresent(bodyDeclaration -> {
-          final var initializerDeclaration = bodyDeclaration.asInitializerDeclaration();
-          for (final var methodCall : initializerDeclaration.findAll(MethodCallExpr.class)) {
-            if (!methodCall.getNameAsString().equals("register")) {
-              continue;
-            }
-            final var scope = methodCall.getScope();
-            if (scope.isEmpty()) {
-              continue;
-            }
-            final var scopedIds = ids.get(scope.get().toString());
-            if (scopedIds == null) {
-              continue;
-            }
-            final var arguments = methodCall.getArguments();
-            final var packetId = scopedIds.get(arguments.get(0).toString());
-            if (packetId == null) {
-              continue;
-            }
-            final var encodeOnly = arguments.get(arguments.size() - 1)
-                .asMethodCallExpr().getArgument(2);
-            methodCall.addArgument(new MethodCallExpr("map",
-                new IntegerLiteralExpr(toHex(packetId)),
-                new NameExpr(versionConstant),
-                encodeOnly));
-          }
-        });
+      if (ids == null) {
+        continue;
       }
+      final var stateBody = stateEntry.getClassBody().getFirst();
+      stateBody.ifPresent(bodyDeclaration -> {
+        final var initializerDeclaration = bodyDeclaration.asInitializerDeclaration();
+        for (final var methodCall : initializerDeclaration.findAll(MethodCallExpr.class)) {
+          if (!methodCall.getNameAsString().equals("register")) {
+            continue;
+          }
+          final var scope = methodCall.getScope();
+          if (scope.isEmpty()) {
+            continue;
+          }
+          final var scopedIds = ids.get(scope.get().toString());
+          if (scopedIds == null) {
+            continue;
+          }
+          final var arguments = methodCall.getArguments();
+          final var packetId = scopedIds.get(arguments.get(0).toString());
+          if (packetId == null) {
+            continue;
+          }
+          final var mappingBefore = arguments.get(arguments.size() - 1).asMethodCallExpr();
+          final var versionBefore = mappingBefore.getArgument(1).toNameExpr();
+          if (versionBefore.isPresent() && versionBefore.get().getNameAsString().equals(versionConstant)) {
+            mappingBefore.setArgument(0, new IntegerLiteralExpr(toHex(packetId)));
+            continue;
+          }
+
+          methodCall.addArgument(new MethodCallExpr("map",
+              new IntegerLiteralExpr(toHex(packetId)),
+              new NameExpr(versionConstant),
+              mappingBefore.getArgument(2)));
+        }
+      });
     }
     return true;
   }
